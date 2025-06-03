c83f24ba139a211e9cdc2958e5bc8b69a29683f4
123tien-alt <bulubuloa12321@gmail.com>
2025-06-03 22:50:42 +0700

    Sửa lại đúng tên app là tracker

--- iplogger/urls.py
+++ iplogger/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main.views import home
+from tracker.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tracker.urls')),
