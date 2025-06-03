1e35286c334461de048c4e2dff4cb6a2a6e15f95
123tien-alt <bulubuloa12321@gmail.com>
2025-06-03 22:39:36 +0700

    Thêm in IP người truy cập

--- tracker/views.py
+++ tracker/views.py
@@ -18,3 +18,18 @@ def log_ip(request):
     with open("log_ip.txt", "a") as f:
         f.write(f"{datetime.datetime.now()} - IP: {ip}\n")
     return render(request, 'tracker/index.html')
+from django.http import HttpResponse
+
+def home(request):
+    ip = get_client_ip(request)
+    print(f"📡 IP truy cập: {ip}")
+    return HttpResponse(f"Chào bạn! IP của bạn là: {ip}")
+
+def get_client_ip(request):
+    # Lấy IP gốc kể cả khi có proxy
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
