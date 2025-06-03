c7dae1ec49b175edc6c02af4d1d94e6813ce32b7
123tien-alt <bulubuloa12321@gmail.com>
2025-06-03 22:41:01 +0700

    Thêm in IP người truy cập

--- tracker/views.py
+++ tracker/views.py
@@ -5,13 +5,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 import datetime
 
-def get_client_ip(request: HttpRequest):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+
 
 def log_ip(request):
     ip = get_client_ip(request)
